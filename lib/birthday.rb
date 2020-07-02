d6700b9d643ef3847dfea7d62d118c6960aa5ce1
Devin Benson <devinbenson@gmail.com>
2020-07-02 22:22:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
+  message = "" 
+  
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    message =  "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end 
+  message 
 end
 
